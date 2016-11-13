b35b695455473e8bddcbe5f4bc148f3edb6f2305
Jenny Kim <yk494@cornell.edu>
2016-11-13 00:07:14 -0500

    styling

--- counter.js
+++ counter.js
@@ -2,7 +2,7 @@
   'use strict'
 
   var ACCESS_TOKEN = 'EAACEdEose0cBAPCksgZBNQYpZAQCk7Jxr5Km4KseezT2ejMmsPJayzZCJFl5liD7oYDPNYVFAFWUNHg3a7nuI2wm7ACSuM5WDNb9ZB7FTnlueZBo3aheAeNNRecxuDOaObB0o4kntgqS2pBaQGxJ5bvziFCr1KVfLzAS0PisaeQZDZD'
-  var VIDEO_ID = '1640762382887785'
+  var VIDEO_ID = '1640767392887284'
   var URL = 'https://graph.facebook.com/v2.8/' + VIDEO_ID + '/reactions?access_token=' + ACCESS_TOKEN
 
   /**
@@ -47,14 +47,15 @@
    **/
   var updateDOM = function(counts) {
     counts = _.defaults(counts, { LIKE: 0, LOVE: 0 })
+
     var exoCount = counts.LIKE
     var btsCount = counts.LOVE
 
     var $exoCount = document.getElementById('exo-count')
     var $btsCount = document.getElementById('bts-count')
 
-    $exoCount.textContent = exoCount
-    $btsCount.textContent = btsCount
+    $exoCount.textContent = exoCount.toLocaleString()
+    $btsCount.textContent = btsCount.toLocaleString()
   }
 
   window.updateCounts = updateCounts
